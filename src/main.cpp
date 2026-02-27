9637301b5bba3165ca60e7460b335879a46f589b
Narendra-Rajawat <anjisolo009@gmail.com>
2026-02-27 17:51:29 +0530

    Define IR sensor digital pin

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,49 @@
 #include <Arduino.h>
+
 /**
  * @file main.cpp
  * @brief Embedded Obstacle Detection System using IR Sensor
- * @author YOUR_NAME
- * @date YYYY-MM-DD
- *
- * @details
- * Reads digital input from IR sensor
- * and detects obstacle presence.
  */
 
- // TODO 1:
- // Define IR sensor digital pin (Use pin 2)
+// TODO 1:
+// Define IR sensor digital pin (Use pin 2)
 
- // TODO 2:
- // Create variable to store sensor state
+#define IR_PIN 2
+
+// TODO 2:
+// Create variable to store sensor state
+int irState = 0;
 
 void setup() {
 
     // TODO 3:
     // Initialize Serial communication (9600 baud rate)
+    Serial.begin(9600);
 
     // TODO 4:
     // Configure IR pin as INPUT
+    pinMode(IR_PIN, INPUT);
 
     // TODO 5:
     // Print system initialization message
+    Serial.println("IR Obstacle Detection System Initialized");
 }
 
 void loop() {
 
     // TODO 6:
     // Read digital value from IR sensor
+    irState = digitalRead(IR_PIN);
 
     // TODO 7:
     // If obstacle detected
-    //     Print "Obstacle Detected"
-    // Else
-    //     Print "No Obstacle"
+    if (irState == LOW) {
+        Serial.println("Obstacle Detected");
+    } else {
+        Serial.println("No Obstacle");
+    }
 
     // TODO 8:
-    // Add small delay (200–500ms)
+    // Add small delay (300ms)
+    delay(300);
 }
